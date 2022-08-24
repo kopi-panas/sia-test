c9080e182b99228f449f3c4581e5de24af5f190f
kopi-panas <datasave.public@mail.com>
2022-08-24 11:23:04 +0700

    bukubesar

--- SIA-ALWIDAJAYA/clsJurnalUmum.vb
+++ SIA-ALWIDAJAYA/clsJurnalUmum.vb
@@ -73,41 +73,49 @@ Public Class clsJurnalUmum
         End Try
     End Function
 
-    'Public Sub InsertSaldoBulanlalu()
-    '    mPeriode = frmJurnalUmum.lblPeriode.Text
-    '    Try
-    '        Query = "INSERT INTO BukuBesar SELECT tmpSaldoBlnLalu.Periode, '" & "SBL-" & "' & [Periode] AS NoTransaksi, tmpSaldoBlnLalu.TglTransaksi, tmpSaldoBlnLalu.NoPerkiraan, tmpSaldoBlnLalu.Keterangan, tmpSaldoBlnLalu.DK, tmpSaldoBlnLalu.Debet, tmpSaldoBlnLalu.Kredit, tmpSaldoBlnLalu.Status FROM(tmpSaldoBlnLalu) WHERE(((tmpSaldoBlnLalu.Periode) = '" & mPeriode & "') And ((tmpSaldoBlnLalu.Status) = '" & "UnPosted" & "'))"
-    '        daData = New OleDbDataAdapter(Query, conn)
-    '        dsData = New DataSet
-    '        daData.Fill(dsData)
-    '    Catch ex As Exception
-    '    End Try
-    'End Sub
+    Public Sub InsertSaldoBulanlalu()
+        mPeriode = frmJurnalUmum.lblPeriode.Text
+        Try
+            Query = "INSERT INTO BukuBesar SELECT tmpSaldoBlnLalu.Periode, '" & "SBL-" & "' & [Periode] AS NoTransaksi, tmpSaldoBlnLalu.TglTransaksi, tmpSaldoBlnLalu.NoPerkiraan, tmpSaldoBlnLalu.Keterangan, tmpSaldoBlnLalu.DK, tmpSaldoBlnLalu.Debet, tmpSaldoBlnLalu.Kredit, tmpSaldoBlnLalu.Status FROM(tmpSaldoBlnLalu) WHERE(((tmpSaldoBlnLalu.Periode) = '" & mPeriode & "') And ((tmpSaldoBlnLalu.Status) = '" & "UnPosted" & "'))"
+            daData = New OleDbDataAdapter(Query, conn)
+            dsData = New DataSet
+            daData.Fill(dsData)
+        Catch ex As Exception
+        End Try
+    End Sub
 
-    'Public Sub InsertBukuBesar()
-    '    mPeriode = frmJurnalUmum.lblPeriode.Text
-    '    Try
-    '        Query = "SELECT DISTINCT hJurnal.Periode, hJurnal.Status FROM hJurnal WHERE (((hJurnal.Periode)='" & mPeriode & "') AND ((hJurnal.Status)='" & "UnPosted" & "'))"
-    '        daData = New OleDbDataAdapter(Query, conn)
-    '        dsData = New DataSet
-    '        daData.Fill(dsData)
+    Public Sub InsertBukuBesar()
+        mPeriode = frmJurnalUmum.lblPeriode.Text
+        Try
+            Query = "SELECT DISTINCT hJurnal.Periode, hJurnal.Status FROM hJurnal WHERE (((hJurnal.Periode)='" & mPeriode & "') AND ((hJurnal.Status)='" & "UnPosted" & "'))"
+            daData = New OleDbDataAdapter(Query, conn)
+            dsData = New DataSet
+            daData.Fill(dsData)
 
-    '        If dsData.Tables(0).Rows.Count = 0 Then
-    '            InsertSaldoBulanlalu()
-    '        Else
-    '            InsertSaldoBulanlalu()
-    '            Query = "INSERT INTO BukuBesar SELECT hJurnal.Periode, hJurnal.NoTransaksi, hJurnal.TglTransaksi, dJurnal.NoPerkiraan, hJurnal.Keterangan, dJurnal.DK, dJurnal.Debet, dJurnal.Kredit, hJurnal.Status FROM (dJurnal LEFT JOIN hJurnal ON dJurnal.NoTransaksi = hJurnal.NoTransaksi) LEFT JOIN tblMasterPerkiraan ON dJurnal.NoPerkiraan = tblMasterPerkiraan.NoPerkiraan WHERE (((hJurnal.Periode)='" & mPeriode & "') AND ((hJurnal.Status)= '" & "UnPosted" & "'))"
-    '            daData = New OleDbDataAdapter(Query, conn)
-    '            dsData = New DataSet
-    '            daData.Fill(dsData)
-    '        End If
-    '    Catch ex As Exception
-    '    End Try
-    'End Sub
+            If dsData.Tables(0).Rows.Count = 0 Then
+                InsertSaldoBulanlalu()
+            Else
+                InsertSaldoBulanlalu()
+                Query = "INSERT INTO BukuBesar SELECT hJurnal.Periode, hJurnal.NoTransaksi, hJurnal.TglTransaksi, dJurnal.NoPerkiraan, hJurnal.Keterangan, dJurnal.DK, dJurnal.Debet, dJurnal.Kredit, hJurnal.Status FROM (dJurnal LEFT JOIN hJurnal ON dJurnal.NoTransaksi = hJurnal.NoTransaksi) LEFT JOIN tblMasterPerkiraan ON dJurnal.NoPerkiraan = tblMasterPerkiraan.NoPerkiraan WHERE (((hJurnal.Periode)='" & mPeriode & "') AND ((hJurnal.Status)= '" & "UnPosted" & "'))"
+                daData = New OleDbDataAdapter(Query, conn)
+                dsData = New DataSet
+                daData.Fill(dsData)
+            End If
+        Catch ex As Exception
+        End Try
+    End Sub
 
-    'Public Sub UpdateBukuBesar()
-    '    Query = "UPDATE BukuBesar SET "
-    'End Sub
+    Public Sub BukuBesar()
+        mPeriode = frmJurnalUmum.lblPeriode.Text
+        If dsData.Tables(0).Rows.Count = 0 Then
+            InsertBukuBesar()
+        Else
+            KoneksiKeAccess()
+            Dim edit As String = "UPDATE BukuBesar SET NoTransaksi ='" & mNoTransaksi & "',TglTransaksi = '" & mTglTransaksi & "',NoPerkiraan = '" & mNoPerkiraan & "',Keterangan = '" & mKeterangan & "',DK = '" & DK & "',Debet = '" & mDebet & "',Kredit = '" & mKredit & "',Status = '" & "UnPosted" & "' WHERE Periode = '" & mPeriode & "'"
+            Command = New OleDbCommand(edit, CONN)
+            Command.ExecuteNonQuery()
+        End If
+    End Sub
 
     Public Function EditDataHJurnal()
         Try
--- SIA-ALWIDAJAYA/frmJurnalUmum.Designer.vb
+++ SIA-ALWIDAJAYA/frmJurnalUmum.Designer.vb
@@ -47,6 +47,7 @@ Partial Class frmJurnalUmum
         Me.lblKredit = New System.Windows.Forms.Label()
         Me.cmdPreview = New System.Windows.Forms.Button()
         Me.lblDebet = New System.Windows.Forms.Label()
+        Me.cmdPreviewBB = New System.Windows.Forms.Button()
         Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -187,7 +188,7 @@ Partial Class frmJurnalUmum
         Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Top
         Me.GroupBox1.Location = New System.Drawing.Point(0, 0)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(787, 126)
+        Me.GroupBox1.Size = New System.Drawing.Size(783, 126)
         Me.GroupBox1.TabIndex = 161
         Me.GroupBox1.TabStop = False
         '
@@ -328,12 +329,27 @@ Partial Class frmJurnalUmum
         Me.lblDebet.TabIndex = 168
         Me.lblDebet.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         '
+        'cmdPreviewBB
+        '
+        Me.cmdPreviewBB.BackColor = System.Drawing.SystemColors.Control
+        Me.cmdPreviewBB.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdPreviewBB.Image = CType(resources.GetObject("cmdPreviewBB.Image"), System.Drawing.Image)
+        Me.cmdPreviewBB.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.cmdPreviewBB.Location = New System.Drawing.Point(477, 448)
+        Me.cmdPreviewBB.Name = "cmdPreviewBB"
+        Me.cmdPreviewBB.Size = New System.Drawing.Size(85, 34)
+        Me.cmdPreviewBB.TabIndex = 171
+        Me.cmdPreviewBB.Text = "&Preview"
+        Me.cmdPreviewBB.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.cmdPreviewBB.UseVisualStyleBackColor = False
+        '
         'frmJurnalUmum
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(787, 490)
+        Me.ClientSize = New System.Drawing.Size(783, 486)
         Me.ControlBox = False
+        Me.Controls.Add(Me.cmdPreviewBB)
         Me.Controls.Add(Me.txtKredit)
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.cmdEdit)
@@ -387,4 +403,5 @@ Partial Class frmJurnalUmum
     Friend WithEvents lblDebet As System.Windows.Forms.Label
     Friend WithEvents cmdTransaksi As System.Windows.Forms.Button
     Friend WithEvents lblNoTransaksi As System.Windows.Forms.Label
+    Friend WithEvents cmdPreviewBB As System.Windows.Forms.Button
 End Class
